b375bde0d63a284ebe579196705ef9cffdee5a13
Kyle <rhodestraveler@gmail.com>
2018-06-03 23:49:59 -0500

    added todos() to reducers.js

--- reducers.js
+++ reducers.js
@@ -10,6 +10,29 @@ const intialState = {
     todos: []
 }
 
+function todos(state = [], action) {
+    switch (action.type) {
+        case ADD_TODO: 
+            return [
+                ...state,
+                {
+                    text: action.text,
+                    completed: false
+                }
+            ];
+        case TOGGLE_TODO:
+            return state.map((todo, index) => {
+                if (index === action.index) {
+                    return Object.assign({}, todo, {
+                        completed: !todo.completed
+                    });
+                }
+                return todo;
+            });
+        default: return state;
+    }
+}
+
 function todoApp(state = initialState, action) {
     switch (action.type) {
         case SET_VISIBILITY_FILTER:
